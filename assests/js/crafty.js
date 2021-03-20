b7b80906b97aa7cd5eb6cb3449723ece85ddcbc0
MiMi T <truttling.mariah@gmail.com>
2021-03-20 01:26:11 -0400

    tile maps

---
+++ assests/js/crafty.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    //start crafty
+    Crafty.init(400, 320);
+    Crafty.canvas.init();
+
+    Crafty.scene("main", function() {
+        Crafty.background('url(assests/maps/mainmap.png) no-repeat')
+        
+    })
+
+
+
+}
---
+++ assests/tiles/realmain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="realmain" tilewidth="16" tileheight="16" tilecount="336" columns="21">
+ <image source="gentle forest, moonlight palette.png" width="336" height="256"/>
+</tileset>
